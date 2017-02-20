6ad6fd5f247562d6c384c0280ea27ec1514a2270
Devin Beliveau <devinmarieb@gmail.com>
2017-02-20 16:37:32 -0700

    Set up redux folders

--- app/index.js
+++ app/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, Link, IndexRoute, browserHistory, IndexRedirect } from 'react-router'
+import {createStore} from 'redux'
+import { Provider } from 'react-redux'
+
+import allReducers from './reducers/index'
 import Main from './components/Main/Main'
 import Header from './components/Header/Header'
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(allReducers);
 
 import './reset.css'
 
 const router = (
-  <Router history={ browserHistory }>
-    <Route path='/' component={ Main }>
-    </Route>
-  </Router>
+  <Provider store={ store }>
+    <Router history={ browserHistory }>
+      <Route path='/' component={ Main }>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 
