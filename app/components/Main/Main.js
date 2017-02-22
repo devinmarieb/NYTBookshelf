87b6e1959267734876cb4070f682570049133e6d
Devin Beliveau <devinmarieb@gmail.com>
2017-02-21 17:47:59 -0700

    WIP map in main container

--- app/components/Main/Main.js
+++ app/components/Main/Main.js
@@ -8,7 +8,7 @@ import './main-styles.scss'
 
 export default class Main extends Component {
 
-  updateList(list) {
+  updateList(list, e) {
     let bookListRequest =('https://api.nytimes.com/svc/books/v3/lists.json')
     bookListRequest += '?' + $.param({
       'api-key': "d7d1e070d64347eda225f068e97c8d21",
@@ -19,21 +19,41 @@ export default class Main extends Component {
     }).then((result) => {
       let books = result.results
       this.props.handleAPI(books)
-      console.log(books);
+      // console.log(books);
     })
+    //WIP change button color on click
+    // e.target.classList.toggle('button-clicked')
   }
 
+  // getInfo() {
+  //   console.log(this.props.bookList);
+  //   // this.props.bookList.map((book)=> {
+  //   //   return(
+  //   //     <article>
+  //   //       { book.book_details.author }
+  //   //     </article>
+  //   //   )
+  //   // })
+  // }
+
+
+
   render(){
-    console.log(this.props.bookList);
+    // console.log(this.props.bookList);
     return(
       <div>
-        <Button name='Hardcover Fiction' className='button' onClick={ ()=> this.updateList('hardcover-fiction') } />
-        <Button name='Paperback Fiction' className='button' onClick={ ()=> this.updateList('trade-fiction-paperback') } />
-        <Button name='E-Book Fiction' className='button' onClick={ ()=> this.updateList('e-book-fiction') } />
-        <Button name='Hardcover NonFiction' className='button' onClick={ ()=> this.updateList('hardcover-nonfiction') } />
-        <Button name='Paperback NonFiction' className='button' onClick={ ()=> this.updateList('paperback-nonfiction') } />
-        <Button name='E-Book NonFiction' className='button' onClick={ ()=> this.updateList('e-book-nonfiction') } />
-        <Button name='Young Adult Hardcover' className='button' onClick={ ()=> this.updateList('young-adult-hardcover') } />
+        <Button name='Hardcover Fiction' className='button' onClick={ (e)=> this.updateList('hardcover-fiction', e) } />
+        <Button name='Paperback Fiction' className='button' onClick={ (e)=> this.updateList('trade-fiction-paperback', e) } />
+        <Button name='E-Book Fiction' className='button' onClick={ (e)=> this.updateList('e-book-fiction', e) } />
+        <Button name='Hardcover NonFiction' className='button' onClick={ (e)=> this.updateList('hardcover-nonfiction', e) } />
+        <Button name='Paperback NonFiction' className='button' onClick={ (e)=> this.updateList('paperback-nonfiction', e) } />
+        <Button name='E-Book NonFiction' className='button' onClick={ (e)=> this.updateList('e-book-nonfiction', e) } />
+        <Button name='Young Adult Hardcover' className='button' onClick={ (e)=> this.updateList('young-adult-hardcover', e) } />
+
+        {/* <section>
+          { this.getInfo() }
+        </section> */}
+
       </div>
     )
   }
--- app/containers/Main-container.js
+++ app/containers/Main-container.js
@@ -4,14 +4,26 @@ import { addBooks } from '../actions'
 import Main from '../components/Main/Main'
 
 const mapStateToProps = (state) => {
-  console.log(state);
+  // console.log(state);
   return { bookList: state.mainReducer.books }
+}
+
+const getBookDetails = (books) => {
+  let nbooks = books.map((book)=> {
+    return(
+      book.book_details.map((info)=> {
+        return(
+          console.log(info.title, info.author, info.description, info.primary_isbn13)
+        )
+      })
+    )
+  })
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
     handleAPI: (books) => {
-      dispatch(addBooks(books))
+      dispatch(addBooks(getBookDetails(books)))
     }
   }
 }
