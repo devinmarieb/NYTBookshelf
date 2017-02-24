714434d7bab4dff3f8acb48f713ec90830c89778
Devin Beliveau <devinmarieb@gmail.com>
2017-02-24 11:33:50 -0700

    Make new noBookCover to match color scheme, add amazon link to bottom of book

--- app/components/Main/Main.js
+++ app/components/Main/Main.js
@@ -60,7 +60,8 @@ export default class Main extends Component {
             <article className='individual-book'>
               <div className='book-image-container'>
                 <Button name='&#9733;' className='star-button' onClick={()=> console.log('click')} />
-                <a href={book.amazon_product_url} target='_blank'><img src={`http://covers.openlibrary.org/b/isbn/${info.primary_isbn13}-L.jpg`} className='book-image'/></a>
+                <img src={`http://covers.openlibrary.org/b/isbn/${info.primary_isbn13}-L.jpg`} className='book-image'/>
+                <a href={book.amazon_product_url} target='_blank' className='amazon-url'>Find on Amazon</a>
               </div>
               <div className='info-text'>
                 <h1 className='book-title'>{ info.title }</h1>
