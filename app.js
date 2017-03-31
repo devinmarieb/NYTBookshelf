142f23db8dbe52ca83792b3f81e7fcc77f8282b4
Devin Beliveau <devinmarieb@gmail.com>
2017-03-30 20:32:54 -0600

    Add env

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Server = require('./server.js')
-const port = (process.env.PORT || 8080)
+// const port = (process.env.PORT || 8080)
 const app = Server.app()
 
-app.listen(port)
+app.listen(process.env.PORT || 5000)
 console.log(`Listening at http://localhost:${port}`)
