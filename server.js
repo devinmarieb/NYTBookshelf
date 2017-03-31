ae1f7e28ffa19ec042a9a4fc205ae2cdc3239349
Devin Beliveau <devinmarieb@gmail.com>
2017-03-31 16:38:01 -0600

    Change static

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ module.exports = {
   app: function () {
     const app = express()
     const indexPath = path.join(__dirname, './build/index.html')
-    const publicPath = express.static(path.join(__dirname, '../images'))
+    // const publicPath = express.static(path.join(__dirname, '../build'))
 
-    app.use('/build', publicPath)
+    app.use(express.static(path.join(__dirname, 'build')))
     app.get('/', function (_, res) { res.sendFile(indexPath) })
 
     return app
